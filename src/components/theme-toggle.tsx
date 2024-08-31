a109474f14f33891531ac850663d766cab946c37
Kuiliang Zhang <z1219202167@outlook.com>
2024-08-31 00:31:42 -0700

    style: remove unnecessary useState

--- src/components/theme-toggle.tsx
+++ src/components/theme-toggle.tsx
@@ -10,14 +10,11 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
+import { useHydration } from "@/hooks/use-hydration";
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme();
-  const [showBadge, setShowBadge] = React.useState(false);
-
-  React.useEffect(() => {
-    setShowBadge(theme === "system");
-  }, [theme]);
+  const hydrated = useHydration();
 
   return (
     <DropdownMenu>
@@ -25,7 +22,7 @@ export function ThemeToggle() {
         <Button variant="outline" size="icon" className="relative">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          {showBadge && <AutoThemeBadge />}
+          {hydrated && theme === "system" && <AutoThemeBadge />}
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
---
+++ src/hooks/use-hydration.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+export function useHydration() {
+  const [hydrated, setHydrated] = useState(false);
+  useEffect(() => {
+    setHydrated(true);
+  }, []);
+  return hydrated;
+}
