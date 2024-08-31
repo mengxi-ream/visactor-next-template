870f936e2acd462bad522bbf280b8efead1ddfd8
Kuiliang Zhang <z1219202167@outlook.com>
2024-08-31 00:41:49 -0700

    feat: change visactor to link

--- src/components/nav/side-nav/components/visactor.tsx
+++ src/components/nav/side-nav/components/visactor.tsx
@@ -1,14 +1,19 @@
+import Link from "next/link";
 import { VisActorLogo } from "@/components/icons";
 
 export default function VisActor() {
   return (
-    <div className="relative my-2 flex flex-col items-center justify-center gap-y-2 px-4 py-4">
+    <Link
+      href="https://visactor.io"
+      target="_blank"
+      className="relative my-2 flex flex-col items-center justify-center gap-y-2 px-4 py-4"
+    >
       <div className="dot-matrix absolute left-0 top-0 -z-10 h-full w-full" />
       <span className="text-xs text-muted-foreground">Powered by</span>
       <div className="flex items-center space-x-2">
         <VisActorLogo size={24} />
         <span className="text-md text-accent-foreground">VisActor</span>
       </div>
-    </div>
+    </Link>
   );
 }
